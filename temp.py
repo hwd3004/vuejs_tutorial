9958daa022e20babba90ca12f4868cc96be24c0e
liufeier <hwd3004@gmail.com>
2022-01-28 11:44:05 +0900

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,13 @@
+# class CustomException(Exception):
+#     def __str__(self):
+#         return "CustomException 발생"
+
+# try:
+
+#     raise CustomException()
+# except CustomException as e:
+#     print(e)
+
+text = 'asdasd'
+
+print(len(text))
